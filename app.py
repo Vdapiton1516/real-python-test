6e0b6ab91c69dc8e7da089974c44afbd76eb6ddb
Vdapiton1516 <Vdapiton.work1516@gmail.com>
2019-04-12 16:41:05 +0800

    flas-hello-world

--- app.py
+++ app.py
@@ -3,14 +3,56 @@
 from flask import Flask
 # create the application object
 app = Flask(__name__)
+
+# error handling
+app.config["DEBUG"] = True
+
 # use the decorator pattern to
 # link the view function to a url
 @app.route("/")
 @app.route("/hello")
 @app.route("/helloworld")
+
 # define the view using a function, which returns a string
 def hello_world():
     return "Hello, World!"
+
+
+#Dynamic Routes
+@app.route("/test1")
+def testing():
+    return "Testing World!!!!!!!!"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+    # dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "vincent" :
+        return "Hello, {}".format(name)
+    else :
+        return "Not Found", 404
+
+
+
 # start the development server using the run() method
+
 if __name__ == "__main__":
     app.run()
